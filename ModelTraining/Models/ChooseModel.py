f242b602e8788c3843098bf96d4e4bc46940a166
someAdjectiveNoun <anjaniintern1991@gmail.com>
2021-09-10 14:43:18 +0200

    updated

--- ModelTraining/Models/ChooseModel.py
+++ ModelTraining/Models/ChooseModel.py
@@ -68,7 +68,7 @@ from Models.semantic_crf import SemanticCRF
 ##################################################################################
 
 def choose_tokenizer_type(pretrained_model):
-    
+
     print('Loading BERT tokenizer...')
     if 'bert' in pretrained_model and 'bio' not in pretrained_model and 'sci' not in pretrained_model:
         tokenizer_ = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
@@ -81,6 +81,9 @@ def choose_tokenizer_type(pretrained_model):
 
     elif 'scibert' in pretrained_model:
         tokenizer_ = AutoTokenizer.from_pretrained("allenai/scibert_scivocab_uncased")
+
+    else:
+        print('Please enter correct tokenizer name...')
 
     return tokenizer_
 
