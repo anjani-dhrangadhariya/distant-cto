968e636df8beab91aa8c64d7ada9479dbc2380d0
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-12-05 19:08:58 +0100

    assertion test added

--- CandidateGeneration/MergeAnnot.py
+++ CandidateGeneration/MergeAnnot.py
@@ -1,12 +1,13 @@
 
-def mergeOldNew(main_dict, key_t, annot_type, ds_annot):
+def mergeOldNew(main_dict, key_t, annot_type, input_annot):
 
     if annot_type not in main_dict[key_t]['annot']:
-        main_dict[key_t]['annot']['ds'] = ds_annot
+        main_dict[key_t]['annot']['ds'] = input_annot
     else:
         merge_to = main_dict[key_t]['annot']['ds']
-        merged_annot = [0] * len(ds_annot)
-        for counter, (o_a, n_a) in enumerate(zip( merge_to, ds_annot  )):
+        merged_annot = [0] * len(input_annot)
+        assert len(merge_to) == len(input_annot)
+        for counter, (o_a, n_a) in enumerate(zip( merge_to, input_annot  )):
             chosen_annot = max( int(o_a), int(n_a) )
             merged_annot[counter] = chosen_annot
 
