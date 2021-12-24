f4e30bb30d833263305b27fa1584cec40bbcac74
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-12-24 11:19:00 +0100

    improved flow control

--- ModelTraining/ReadCandidateData.py
+++ ModelTraining/ReadCandidateData.py
@@ -8,6 +8,8 @@ import logging
 import os
 import pdb
 import random
+from statistics import mean
+import statistics
 import sys
 import time
 
@@ -15,6 +17,8 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+
+from collections import Counter
 
 from Utilities.experiment_arguments import *
 from DataBuilders.ContextualVectorBuilder import *
@@ -38,6 +42,7 @@ def readRawCandidates( list_NCT, annotation ):
     tokens = []
     labels = []
     pos = []
+    lengths = []
 
     with open(list_NCT, 'r', encoding='latin1') as NCT_ids_file:
 
@@ -48,15 +53,16 @@ def readRawCandidates( list_NCT, annotation ):
 
                 for target_key, target in annot.items():
 
-                    if 'id' not in target_key:
+                    if 'id' not in target_key and annotation in target and len(target['tokens']) >= 5: #and 'ds_np_fuzz' in target 
 
                         nct_ids.append( id_ )
                         tokens.append( target['tokens'] )
                         labels.append( target[annotation] )
                         pos.append( target['pos'] )
+                        lengths.append( len(target['tokens']) )
 
-                if i == 6:
-                    break
+                #if i == 20:
+                #    break
 
     corpus_df = pd.DataFrame(
         {'ids': nct_ids,
@@ -65,13 +71,14 @@ def readRawCandidates( list_NCT, annotation ):
         'pos': pos
         })
 
-    df = corpus_df.sample(frac=1).reset_index(drop=True) # Shuffles the dataframe after creation
-    
-    # can delete this one (corpusDf)
-    del corpus_df
-    gc.collect() # mark if for garbage collection
+    #Elements_with_frequency = Counter(lengths)
+    #maxlen = Elements_with_frequency.most_common()
+    #maxlen = statistics.mean( lengths )
+    #maxlen = max( lengths )
+    maxlen = 100
+    print(' The max length for sequences in the raw candidates is: ', maxlen)
 
-    return df
+    return corpus_df, maxlen
 
 def readManuallyAnnoted( input_file_path ):
 
@@ -101,13 +108,7 @@ def readManuallyAnnoted( input_file_path ):
         'pos': pos
         })
 
-    df = corpus_df.sample(frac=1).reset_index(drop=True) # Shuffles the dataframe after creation
-    
-    # can delete this one (corpusDf)
-    del corpus_df
-    gc.collect() # mark if for garbage collection
-
-    return df
+    return corpus_df
 
 
 def fetchAndTransformCandidates():
@@ -118,8 +119,15 @@ def fetchAndTransformCandidates():
     args = getArguments() # get all the experimental arguments
 
     start_candidate_reading = time.time()
-    raw_candidates = readRawCandidates( args.rawcand_file, annotation=args.annot )
+    raw_candidates, MAX_LEN = readRawCandidates( args.rawcand_file, annotation=args.annot )
     print("--- Took %s seconds to read the raw weakly annotated candidates ---" % (time.time() - start_candidate_reading))
+
+    if args.train_data == 'distant-cto':
+        maxlen = MAX_LEN
+    if args.train_data == 'ebm-pico':
+        maxlen = args.max_len
+    if args.train_data == 'combined':
+        maxlen = MAX_LEN
 
     # Retrieve EBM-PICO dataset here
     start_manual_reading = time.time()
@@ -138,14 +146,14 @@ def fetchAndTransformCandidates():
 
     start_candidate_transformation = time.time()
     tokenizer, model = choose_tokenizer_type( args.embed )
-    input_embeddings, input_labels, input_masks, input_pos, tokenizer = getContextualVectors( raw_candidates, tokenizer, args.embed, args.max_len, le_pos )
+    input_embeddings, input_labels, input_masks, input_pos, tokenizer = getContextualVectors( raw_candidates, tokenizer, args.embed, maxlen, le_pos )
     assert len( input_embeddings ) == len( raw_candidates )
     print("--- Took %s seconds to transform the raw weakly annotated candidates ---" % (time.time() - start_candidate_transformation))
 
     start_manual_transformation = time.time()
-    ebm_nlp_embeddings, ebm_nlp_labels, ebm_nlp_masks, ebm_nlp_pos, tokenizer = getContextualVectors( ebm_nlp, tokenizer, args.embed, args.max_len, le_pos )
-    ebm_gold_embeddings, ebm_gold_labels, ebm_gold_masks, ebm_gold_pos, tokenizer = getContextualVectors( ebm_gold, tokenizer, args.embed, args.max_len, le_pos )
-    hilfiker_embeddings, hilfiker_labels, hilfiker_masks, hilfiker_pos, tokenizer = getContextualVectors( hilfiker, tokenizer, args.embed, args.max_len, le_pos )
+    ebm_nlp_embeddings, ebm_nlp_labels, ebm_nlp_masks, ebm_nlp_pos, tokenizer = getContextualVectors( ebm_nlp, tokenizer, args.embed, maxlen, le_pos )
+    ebm_gold_embeddings, ebm_gold_labels, ebm_gold_masks, ebm_gold_pos, tokenizer = getContextualVectors( ebm_gold, tokenizer, args.embed, maxlen, le_pos )
+    hilfiker_embeddings, hilfiker_labels, hilfiker_masks, hilfiker_pos, tokenizer = getContextualVectors( hilfiker, tokenizer, args.embed, maxlen, le_pos )
     print("--- Took %s seconds to transform the manually annotated datasets ---" % (time.time() - start_manual_transformation))
 
     candidates_df = raw_candidates.assign(embeddings = pd.Series(input_embeddings).values, label_pads = pd.Series(input_labels).values, attn_masks = pd.Series(input_masks).values, inputpos = pd.Series(input_pos).values) # assign the padded embeddings to the dataframe
@@ -153,6 +161,7 @@ def fetchAndTransformCandidates():
     ebm_gold_df = ebm_gold.assign(embeddings = pd.Series(ebm_gold_embeddings).values, label_pads = pd.Series(ebm_gold_labels).values, attn_masks = pd.Series(ebm_gold_masks).values, inputpos = pd.Series(ebm_gold_pos).values)
     hilfiker_df = hilfiker.assign(embeddings = pd.Series(hilfiker_embeddings).values, label_pads = pd.Series(hilfiker_labels).values, attn_masks = pd.Series(hilfiker_masks).values, inputpos = pd.Series(hilfiker_pos).values)
 
-    del input_embeddings, input_labels, input_masks, input_pos     # Delete the large variables
+    del input_embeddings, input_labels, input_masks, input_pos, ebm_nlp_embeddings, ebm_nlp_labels, ebm_nlp_masks, ebm_nlp_pos, ebm_gold_embeddings, ebm_gold_labels, ebm_gold_masks, ebm_gold_pos, hilfiker_embeddings, hilfiker_labels, hilfiker_masks, hilfiker_pos, raw_candidates, ebm_nlp, ebm_gold, hilfiker  # Delete the large variables
+    gc.collect()
 
     return candidates_df, ebm_nlp_df, ebm_gold_df, hilfiker_df, args, tokenizer, model
